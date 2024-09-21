c471b3850bbfccbaf3fdd1a8c70780e86703bd27
Zubarev <91302806+zubarevd@users.noreply.github.com>
2024-09-21 13:56:34 +0500

    PR3
    Описание приложения
    Приложение предназначено для управления информацией о фильмах. Оно включает в себя следующие поля:
    Название фильма: текстовое поле для ввода названия фильма.
    Режиссёр: текстовое поле для указания имени режиссёра.
    Год выпуска: числовое поле для ввода года выпуска фильма.
    Валидация
    При вводе данных необходимо учитывать, что поле "Год выпуска" должно содержать только числовое значение. Если пользователь введёт нечисловое значение, приложение должно выдавать сообщение об ошибке и запрашивать повторный ввод.

---
+++ PR3/PR3/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       public List<Product> Products {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Products = new List<Product>();
+            ProductsDataGrid.ItemsSource = Products;
+        }
+
+        private void AddProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            string productName = ProductNameTextBox.Text;
+            string productPrice = ProductPriceTextBox.Text;
+            string productQuantity = ProductQuantityTextBox.Text;
+
+            Products.Add(new Product
+            {
+                ProductName = productName,
+                ProductPrice = productPrice,
+                ProductQuantity = productQuantity,
+            });
+
+            ProductNameTextBox.Clear();
+            ProductPriceTextBox.Clear();
+            ProductQuantityTextBox.Clear();
+        }
+    }
+
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductPrice { get; set; }
+        public string ProductQuantity { get; set; }
+    }
+}
+
+
